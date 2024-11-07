940bdb270d5d331c54a81875ee6dd30155842fe3
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-07 16:06:52 +0600

    5- Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,27 @@
-import { useState } from "react";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  // const items = ["New Yourk", "Los Angeles", "San Francisco"];
-
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 13, category: "Utilities" },
-    { id: 3, description: "cccc", amount: 123, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 14, category: "Utilities" },
-    { id: 5, description: "fff", amount: 440, category: "Utilities" },
-  ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
 
   return (
-    <div>
-      {/* <ListGroup
-        heading="Miani"
-        items={items}
-        onSelectItem={() => console.log("clicked")}
-      />
-      <Button onClick={() => {}}>My Button</Button>
-      <Like onClick={() => console.log("Clicked")} />
-        <Message/> */}
-      {/* <Form /> */}
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
-    </div>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default App;
